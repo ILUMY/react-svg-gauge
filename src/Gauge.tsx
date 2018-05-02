a6ba102b57af7b7ece288e1676ba7f563ac96deb
Lars Douwe Schuitema <info@larsschuitema.nl>
2018-05-02 14:37:23 +0200

    fix whitespace on top & remove labels

--- src/Gauge.tsx
+++ src/Gauge.tsx
@@ -12,15 +12,11 @@ export interface GaugeProps {
   height: number,
   color: string,
   symbol: string,
-  backgroundColor: string,
-  topLabelStyle: React.CSSProperties,
-  valueLabelStyle: React.CSSProperties,
-  minMaxLabelStyle: React.CSSProperties
+	backgroundColor: string
 }
 
 export default class Gauge extends React.Component<GaugeProps> {
 	static defaultProps = {
-		label: "React SVG Gauge",
 		min: 0,
 		max: 100,
 		value: 40,
@@ -29,9 +25,6 @@ export default class Gauge extends React.Component<GaugeProps> {
 		color: '#fe0400',
 		symbol: '',
 		backgroundColor: "#edebeb",
-		topLabelStyle: {textAnchor: "middle", fill:"#999999", stroke: "none", fontStyle: "normal",fontVariant: "normal", fontWeight: 'bold', fontStretch: 'normal', lineHeight: 'normal', fillOpacity: 1},
-		valueLabelStyle: {textAnchor: "middle", fill:"#010101", stroke: "none", fontStyle: "normal", fontVariant: "normal", fontWeight: 'bold', fontStretch: 'normal', lineHeight: 'normal', fillOpacity: 1},
-		minMaxLabelStyle: {textAnchor: "middle", fill:"#999999", stroke: "none", fontStyle: "normal",fontVariant: "normal", fontWeight: 'normal', fontStretch: 'normal', fontSize: 20, lineHeight: 'normal', fillOpacity: 1}
 	};
 
 	_getPathValues = (value:number) => {
@@ -46,7 +39,7 @@ export default class Gauge extends React.Component<GaugeProps> {
 		let Ri = Ro - this.props.width / 6.666666666666667;
 
 		let Cx = this.props.width / 2 + dx;
-		let Cy = this.props.height / 1.25 + dy;
+		let Cy = this.props.height / 2 + dy;
 
 		let Xo = this.props.width / 2 + dx + Ro * Math.cos(alpha);
 		let Yo = this.props.height - (this.props.height - Cy) - Ro * Math.sin(alpha);
@@ -72,13 +65,6 @@ export default class Gauge extends React.Component<GaugeProps> {
   private uniqueFilterId:string;
 
 	render() {
-		let topLabelStyle = (this.props.topLabelStyle.fontSize
-				? this.props.topLabelStyle
-				: {...this.props.topLabelStyle, fontSize: (this.props.width / 10) });
-		let valueLabelStyle = (this.props.valueLabelStyle.fontSize
-				? this.props.valueLabelStyle
-				: {...this.props.valueLabelStyle, fontSize: (this.props.width / 5) });
-		let { Cx, Ro, Ri, Xo, Cy, Xi } = this._getPathValues(this.props.max);
 		if(!this.uniqueFilterId) this.uniqueFilterId = "filter_"+ uniqueId++;
 		return (
 				<svg height="100%" version="1.1" width="100%" xmlns="http://www.w3.org/2000/svg" style={{width: this.props.width, height: this.props.height, overflow: 'hidden', position: 'relative', left: 0, top: 0}}>
@@ -94,18 +80,6 @@ export default class Gauge extends React.Component<GaugeProps> {
 					</defs>
 					<path fill={this.props.backgroundColor} stroke="none" d={this._getPath(this.props.max)} filter={"url(#" + this.uniqueFilterId + ")"} />
 					<path fill={this.props.color} stroke="none" d={this._getPath(this.props.value)} filter={"url(#" + this.uniqueFilterId + ")"} />
-					<text x={this.props.width / 2} y={this.props.height / 8} textAnchor="middle" style={topLabelStyle}>
-						{ this.props.label }
-					</text>
-					<text x={this.props.width / 2} y={this.props.height / 5 * 4} textAnchor="middle" style={valueLabelStyle}>
-						{ this.props.value + this.props.symbol  }
-					</text>
-					<text x={((Cx - Ro) + (Cx - Ri)) / 2} y={Cy + 25} textAnchor="middle" style={this.props.minMaxLabelStyle}>
-						{this.props.min}
-					</text>
-					<text x={(Xo + Xi)/2} y={Cy + 25} textAnchor="middle" style={this.props.minMaxLabelStyle}>
-						{this.props.max}
-					</text>
 				</svg>
 		);
 	}
